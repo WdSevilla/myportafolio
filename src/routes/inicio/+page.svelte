698d27ef929e0a703699907cf9438ae26b1b60a5
Wil <sevillawil8@gmail.com>
2024-06-25 10:01:39 -0600

    Cambio en el titulo del inicio

--- src/routes/inicio/+page.svelte
+++ src/routes/inicio/+page.svelte
@@ -3,7 +3,7 @@
 <div class="bg-back bg-cover pt-[60%] lg:pt-[24%] h-screen">
 <div class="grid items-center justify-center">
     <h1 class="text-white text-7xl pb-6">Welcome</h1>
-    <p class="text-white text-3xl">I'm IT Technician Support & Jr.Frontend Developer</p>
+    <p class="text-white text-3xl">I'm Web Developer & IT Technician Support </p>
   </div>
 </div>
 </div>
