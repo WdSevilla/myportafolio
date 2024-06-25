d79fa8574f5c287bb123c3079daf68d4067f3468
Wil <sevillawil8@gmail.com>
2024-06-24 23:46:42 -0600

    cambio en la imagen

--- src/routes/inicio/+page.svelte
+++ src/routes/inicio/+page.svelte
@@ -1,9 +1,10 @@
 <script></script>
 <div>
-<div class="bg-back bg-cover pt-[60%] lg:pt-[24%] h-screen">
+  <img src="" alt="" class="bg-back bg-cover pt-[60%] lg:pt-[24%] h-screen">
+<!-- <div class="bg-back bg-cover pt-[60%] lg:pt-[24%] h-screen">
 <div class="grid items-center justify-center">
     <h1 class="text-white text-7xl pb-6">Welcome</h1>
     <p class="text-white text-3xl">I'm IT Technician Support & Jr.Frontend Developer</p>
   </div>
-</div>
+</div> -->
 </div>
