ffeeaaba2eea6e24d2e7d3edc351766868ea87a9
Wil <sevillawil8@gmail.com>
2024-06-25 13:18:40 -0600

    se agrega la animacion de la funcion de escritura

--- src/routes/inicio/+page.svelte
+++ src/routes/inicio/+page.svelte
@@ -1,9 +1,82 @@
-<script></script>
-<div>
+<script>
+  import { onMount } from 'svelte';
+
+  let title = '';
+  let subtitle = '';
+  const text = 'Bienvenido a mi sitio web';
+  const text2 = 'Soy desarrollador web';
+  let index = 0;
+  let index2 = 0;
+  let isTitleComplete = false;
+
+  function typeEffect() {
+    if (index < text.length) {
+      title += text.charAt(index);
+      index++;
+      setTimeout(typeEffect, 100); // Ajusta el tiempo para cambiar la velocidad de la escritura
+    } else {
+      isTitleComplete = true;
+      setTimeout(typeEffect2, 500); // Espera 500ms antes de iniciar el segundo efecto de escritura
+    }
+  }
+
+  function typeEffect2() {
+    if (index2 < text2.length) {
+      subtitle += text2.charAt(index2);
+      index2++;
+      setTimeout(typeEffect2, 100); // Ajusta el tiempo para cambiar la velocidad de la escritura
+    }
+  }
+
+  onMount(() => {
+    typeEffect();
+  });
+</script>
+
+<style>
+  #typing-title {
+    font-family: 'Courier New', Courier, monospace;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  #typing-title.complete::after {
+    content: '';
+  }
+
+  #typing-title:not(.complete)::after {
+    content: '|';
+    animation: blink 0.7s steps(2, start) infinite;
+    display: inline-block;
+    margin-left: 2px;
+  }
+
+  #typing-subtitle {
+    font-family: 'Courier New', Courier, monospace;
+    white-space: nowrap;
+    overflow: hidden;
+  }
+
+  .cursor-blink::after {
+    content: '|';
+    animation: blink 0.7s steps(2, start) infinite;
+    display: inline-block;
+    margin-left: 2px;
+  }
+
+  @keyframes blink {
+    0%, 50% {
+      opacity: 1;
+    }
+    50.01%, 100% {
+      opacity: 0;
+    }
+  }
+</style>
+
 <div class="bg-back bg-cover pt-[60%] lg:pt-[24%] h-screen">
-<div class="grid items-center justify-center">
-    <h1 class="text-white text-7xl pb-6">Welcome</h1>
-    <p class="text-white text-3xl">I'm Web Developer & IT Technician Support </p>
+  <div class="grid items-center justify-center">
+    <h1 id="typing-title" class="text-white text-7xl pb-6 {isTitleComplete ? 'complete' : ''}">{title}</h1>
+    <p id="typing-subtitle" class="text-white text-3xl {isTitleComplete ? 'cursor-blink' : ''}">{subtitle}</p>
   </div>
-</div>
 </div>
